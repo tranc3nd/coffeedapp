cd7205f2c370ef9b4e0c70dabc29f4c04d1a165a
tranc3nd <jingco@gmail.com>
2015-06-03 15:59:06 -0700

    ready to push

--- coffeedapp/urls.py
+++ coffeedapp/urls.py
@@ -17,5 +17,9 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('core.urls')),
 ]
+
+
